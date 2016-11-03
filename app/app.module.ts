7d392dae6f89d9b4cfd87118fb915f121b04eb06
Longfld <Longfld@users.noreply.github.com>
2016-11-03 17:09:17 +1100

    Add angular material2

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import { MaterialModule } from '@angular/material';
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, MaterialModule.forRoot()],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
