09ead87c0c65b6c7b3e4ccd33da8d77d19595ae2
Longfld <Longfld@users.noreply.github.com>
2017-04-27 09:37:56 +1000

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import 'rxjs/Rx';
+import 'hammerjs/hammer';
+
 @NgModule({
   imports:      [ BrowserModule, BrowserAnimationsModule ],
   declarations: [ AppComponent ],
