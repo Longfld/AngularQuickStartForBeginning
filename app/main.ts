18a247297b3ad902ed805a2c4c074fd1e0afdbf0
Longfld <Longfld@users.noreply.github.com>
2017-04-27 09:09:43 +1000

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
+import 'rxjs/Rx';
+import 'hammerjs/hammer';
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
