86c0001912ad855ed1ea44b08dffb5ac070bd0e9
Longfld <Longfld@users.noreply.github.com>
2017-06-26 10:24:43 +1000

    Add datepicker

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule, MdNativeDateModule} from '@angular/material';
 
 import 'rxjs/Rx';
 import 'hammerjs/hammer';
 
 @NgModule({
-  imports:      [ BrowserModule, BrowserAnimationsModule ],
+  imports:      [ BrowserModule, MaterialModule, MdNativeDateModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
