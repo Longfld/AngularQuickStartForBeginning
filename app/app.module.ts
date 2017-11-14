bb67b2c9d27a4731356dbd4a552f731a47bb1ecc
Longfld <Longfld@users.noreply.github.com>
2017-11-14 15:21:34 +1100

    Angular Material 5

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
-import {MaterialModule, MdNativeDateModule} from '@angular/material';
-
-import 'rxjs/Rx';
+import {NgModule }      from '@angular/core';
+import {BrowserModule } from '@angular/platform-browser';
+import {AppComponent }   from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MyMaterialModule} from './my.material.module';
 import 'hammerjs/hammer';
 
 @NgModule({
-  imports:      [ BrowserModule, MaterialModule, MdNativeDateModule],
+  imports:      [ BrowserModule, BrowserAnimationsModule,
+                  FormsModule, HttpModule,
+                  MyMaterialModule,ReactiveFormsModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
