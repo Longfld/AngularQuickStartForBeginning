886afb4b40e7e543d8a29aa281eef0b77946d7f5
Longfld <Longfld@users.noreply.github.com>
2017-11-14 15:18:57 +1100

    Angular material 5

--- systemjs.config.js
+++ systemjs.config.js
@@ -13,20 +13,41 @@
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
       '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/common/http': 'npm:@angular/common/bundles/common-http.umd.js',
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      
+
       '@angular/animations': 'node_modules/@angular/animations/bundles/animations.umd.min.js',
-      '@angular/animations/browser':'node_modules/@angular/animations/bundles/animations-browser.umd.js',
+      '@angular/animations/browser': 'node_modules/@angular/animations/bundles/animations-browser.umd.js',
       '@angular/platform-browser/animations': 'node_modules/@angular/platform-browser/bundles/platform-browser-animations.umd.js',
-      
-       '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
+
+      '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
+      '@angular/cdk': 'npm:@angular/cdk/bundles/cdk.umd.js',
+
+
+      // CDK Secondary entry points
+      '@angular/cdk/a11y': 'npm:@angular/cdk/bundles/cdk-a11y.umd.js',
+      '@angular/cdk/accordion': 'npm:@angular/cdk/bundles/cdk-accordion.umd.js',
+      '@angular/cdk/bidi': 'npm:@angular/cdk/bundles/cdk-bidi.umd.js',
+      '@angular/cdk/coercion': 'npm:@angular/cdk/bundles/cdk-coercion.umd.js',
+      '@angular/cdk/collections': 'npm:@angular/cdk/bundles/cdk-collections.umd.js',
+      '@angular/cdk/keycodes': 'npm:@angular/cdk/bundles/cdk-keycodes.umd.js',
+      '@angular/cdk/observers': 'npm:@angular/cdk/bundles/cdk-observers.umd.js',
+      '@angular/cdk/overlay': 'npm:@angular/cdk/bundles/cdk-overlay.umd.js',
+      '@angular/cdk/platform': 'npm:@angular/cdk/bundles/cdk-platform.umd.js',
+      '@angular/cdk/portal': 'npm:@angular/cdk/bundles/cdk-portal.umd.js',
+      '@angular/cdk/scrolling': 'npm:@angular/cdk/bundles/cdk-scrolling.umd.js',
+      '@angular/cdk/table': 'npm:@angular/cdk/bundles/cdk-table.umd.js',
+      '@angular/cdk/stepper': 'npm:@angular/cdk/bundles/cdk-stepper.umd.js',
+      '@angular/cdk/layout': 'npm:@angular/cdk/bundles/cdk-layout.umd.js',
+
 
       // other libraries
-      'rxjs':                      'npm:rxjs',
+      'rxjs': 'npm:rxjs',
+      'tslib': 'npm:tslib/tslib.js',
       'hammerjs': 'npm:hammerjs'
     },
     // packages tells the System loader how to load when no filename and/or no extension
@@ -36,11 +57,13 @@
         defaultExtension: 'js'
       },
       rxjs: {
-        defaultExtension: 'js'
+        main: 'index'
       },
-      hammerjs:  { 
-                main: './hammer.js', 
-                defaultExtension: 'js'}
+      hammerjs: {
+        main: './hammer.js',
+        defaultExtension: 'js'
       }
+    }
   });
 })(this);
+
